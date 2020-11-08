7ce7ac45cf1721ed8486af0d38fe1469013f9562
Dominik Elsbroek <de@elsbroek.com>
2020-11-08 20:36:13 +0100

    added copyright

--- cmd_options.rb
+++ cmd_options.rb
@@ -1,3 +1,29 @@
+# Copyright 2020 Dominik Elsbroek. All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification, are
+# permitted provided that the following conditions are met:
+# 
+#    1. Redistributions of source code must retain the above copyright notice, this list of
+#       conditions and the following disclaimer.
+# 
+#    2. Redistributions in binary form must reproduce the above copyright notice, this list
+#       of conditions and the following disclaimer in the documentation and/or other materials
+#       provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY Dominik Elsbroek ``AS IS'' AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Dominik Elsbroek OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
+# The views and conclusions contained in the software and documentation are those of the
+# authors and should not be interpreted as representing official policies, either expressed
+# or implied, of Dominik Elsbroek
+
 require 'optparse'
 require 'optparse/time'
 require 'ostruct'
@@ -28,7 +54,6 @@ class OptparseExample
       debug_option(parser)
       verbose_option(parser)
       quiet_option(parser)
-      # add additional options
 
       parser.separator ""
       parser.separator "Common options:"
@@ -37,8 +62,7 @@ class OptparseExample
         puts parser
         exit
       end
-      # Another typical switch to print the version.
-      parser.on_tail("--version", "Show version") do
+         parser.on_tail("--version", "Show version") do
         puts "version: #{Version}"
         exit
       end
@@ -82,13 +106,7 @@ class OptparseExample
     end
   end
 
-  #
-  # Return a structure describing the options.
-  #
   def parse(args)
-    # The options specified on the command line will be collected in
-    # *options*.
-
     @options = ScriptOptions.new
     @args = OptionParser.new do |parser|
       @options.define_options(parser)
@@ -98,6 +116,6 @@ class OptparseExample
   end
 
   attr_reader :parser, :options
-end  # class OptparseExample
+end
 
 
